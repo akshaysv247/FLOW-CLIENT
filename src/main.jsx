14a0cc281ec2a0be01319aa9c7ac7707c23a7749
nelson <akshaysv957@gmail.com>
2023-05-12 19:32:34 +0530

    added hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { HashRouter } from 'react-router-dom';
 import { persistor, store } from './Redux/Store';
 import App from './App';
 import './index.css';
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <HashRouter>
+          <App />
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
