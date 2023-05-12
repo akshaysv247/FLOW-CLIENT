0eeb8c503725d67333ea5e6c3195a763a9273c79
nelson <akshaysv957@gmail.com>
2023-05-12 19:35:46 +0530

    remove hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { HashRouter } from 'react-router-dom';
 import { persistor, store } from './Redux/Store';
 import App from './App';
 import './index.css';
@@ -12,9 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <HashRouter>
-          <App />
-        </HashRouter>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>,
