a5afd525894ba8348666cf3646e44c5d56945d7b
nelson <akshaysv957@gmail.com>
2023-08-07 20:03:43 +0530

    baseURL changed

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://api.flowmusics.online',
+  // baseURL: 'https://api.flowmusics.online',
   // baseURL: 'http://localhost:3002',
+  baseURL: 'https://flow-backend-uyqb.onrender.com',
 });
 
 export default instance;
